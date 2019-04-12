307dee1994b53c029cc557c4e64fbf45069c51b3
Alkyone <alkyone@MacBook-Pro.local>
2019-04-12 13:24:22 +0300

    map update

--- lib/layout/maps.dart
+++ lib/layout/maps.dart
@@ -1,14 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:fluttertoast/fluttertoast.dart';
 import 'dart:async';
 import 'dart:convert';
 import 'package:http/http.dart' as http;
+import 'package:location/location.dart';
 import 'package:sqflite/sqflite.dart';
 import 'package:Marketim/models/liste.dart';
 import 'package:Marketim/utils/database_helper.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
-import 'package:location/location.dart';
+import 'package:location/location.dart' as loc;
+import "package:google_maps_webservice/geocoding.dart";
+
+
+
+
 
 
 
@@ -21,25 +26,26 @@ class Maps extends StatefulWidget  {
 
 class _MapsPageState extends State<Maps> {
   Completer<GoogleMapController> _controller = Completer();
-
+  var location = new loc.Location(); 
   Future LocationInfo() async{
-    
-    LocationData currentLocation;
-
-    var location = new Location();  
-    location.changeSettings(
-      accuracy: LocationAccuracy.HIGH,
+     
+    loc.LocationData currentLocation;
+    await location.changeSettings(
+      accuracy: loc.LocationAccuracy.HIGH,
+      interval: 1000,
     );  
     // Platform messages may fail, so we use a try/catch PlatformException.
     try {
-      currentLocation = await location.getLocation();
+      currentLocation = await location.getLocation();      
       final CameraPosition _myLoc = CameraPosition(
         target: LatLng(currentLocation.latitude, currentLocation.longitude),
-        zoom: 14,
+        zoom: 11,
       );
       
       final GoogleMapController controller = await _controller.future;
       controller.animateCamera(CameraUpdate.newCameraPosition(_myLoc));
+
+       
     
     } on PlatformException catch (e) {
       if (e.code == 'PERMISSION_DENIED') {
@@ -49,45 +55,114 @@ class _MapsPageState extends State<Maps> {
     }
   }
 
+  Future locationInfoDubug() async{
+    var latitude = 38.467441;
+    var longitude = 27.218683;    
+    final CameraPosition _myLoc = CameraPosition(
+        target: LatLng(latitude, longitude),
+        zoom: 11,
+    );
+      
+    final GoogleMapController controller = await _controller.future;
+    
+    controller.animateCamera(CameraUpdate.newCameraPosition(_myLoc));
+    
+  } 
+
+  Future getPlaces() async{    
+    final geocoding = new GoogleMapsGeocoding(apiKey: "AIzaSyBZJ7I2PmkUNq47W7bl6pZyfMU-ixfuEmU");
+    GeocodingResponse response = await geocoding.searchByAddress("edremit");
+    print(response.results.length);  
+    print(response.results[0].formattedAddress);
+    print(response.results[0].geometry.location.lat);
+    print(response.results[0].geometry.location.lng);    
+    
+    GeocodingResponse res = await geocoding.searchByLocation(response.results[0].geometry.location);
+    print(res.results[2].formattedAddress);
+  }
+
   static final CameraPosition _kGooglePlex = CameraPosition(
     target: LatLng(37.42796133580664, -122.085749655962),
-    zoom: 14.4746,
+    zoom: 11,
   );
 
-  static final CameraPosition _kLake = CameraPosition(
-      bearing: 192.8334901395799,
-      target: LatLng(37.43296265331129, -122.08832357078792),
-      tilt: 59.440717697143555,
-      zoom: 19.151926040649414);
-
+  void camStop(){
+    location.onLocationChanged().listen((LocationData currentLocation) {            
+      //print(currentLocation.latitude);
+      //print(currentLocation.longitude);
+    });    
+  }
 
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
        appBar: AppBar(
-        title: Text("Lokasyon Ayarı"),      
+        title: Text("Konum Güncelle"),      
       ),
-      body: GoogleMap(
-        //cameraTargetBounds: CameraTargetBounds.unbounded,
-        myLocationEnabled: true,        
-        mapType: MapType.normal,
-        initialCameraPosition: _kGooglePlex,
-        onMapCreated: (GoogleMapController controller) {
-          _controller.complete(controller);          
-        }
+      body:
+      Container(
+        width: MediaQuery.of(context).size.width,
+        height: MediaQuery.of(context).size.height-250,
+        padding: EdgeInsets.all(15),
+        child:Stack(
+          children:<Widget>[
+            GoogleMap(
+              tiltGesturesEnabled: true,
+              //cameraTargetBounds: CameraTargetBounds.unbounded,
+              myLocationEnabled: false,                 
+              mapType: MapType.normal,
+              onCameraIdle: ()=>camStop(),
+              initialCameraPosition: _kGooglePlex,
+              onMapCreated: (GoogleMapController controller) {
+                _controller.complete(controller);          
+              },        
+            ),
+            Padding(
+              padding: const EdgeInsets.all(5),
+              child: Align(
+                alignment: Alignment.topCenter,
+                child: TextField(                  
+                  decoration: InputDecoration(
+                    border: InputBorder.none,
+                    hintText: "Yer Ara",
+                    fillColor: Colors.white,
+                  )                 
+                ),
+                /*child: FloatingActionButton(                  
+                  onPressed: () => locationInfoDubug(),
+                  materialTapTargetSize: MaterialTapTargetSize.padded,
+                  backgroundColor: Colors.green,
+                  child: const Icon(Icons.location_on, size: 30.0),
+                ),*/
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(5),
+              child: Align(
+                alignment: Alignment.center,
+                child: GestureDetector(
+                  onTap: ()=>locationInfoDubug(),
+                  child: Icon(Icons.location_on,color: Colors.red, size: 40,),
+                ),
+                /*child: FloatingActionButton(                  
+                  onPressed: () => locationInfoDubug(),
+                  materialTapTargetSize: MaterialTapTargetSize.padded,
+                  backgroundColor: Colors.green,
+                  child: const Icon(Icons.location_on, size: 30.0),
+                ),*/
+              ),
+            ),
+          ],
+        ),
       ),
       floatingActionButton: FloatingActionButton.extended(
-        onPressed: _goToTheLake,
-        label: Text('To the lake!'),
-        icon: Icon(Icons.directions_boat),
+        onPressed: (){
+          getPlaces();
+        },
+        label: Text('Güncelle'),
+        icon: Icon(Icons.save),
       ),
     );
-  }
-
-  Future<void> _goToTheLake() async {
-    final GoogleMapController controller = await _controller.future;
-    controller.animateCamera(CameraUpdate.newCameraPosition(_kLake));
-
   }
 
   @override
