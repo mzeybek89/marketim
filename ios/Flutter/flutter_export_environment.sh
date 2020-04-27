ab65b15b0f5742714199c41b1c755756d29c843a
mzeybek89 <mehmetzeybek89@gmail.com>
2020-04-27 22:38:14 +0300

    ios13.4

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,10 +1,12 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=C:\Flutter"
-export "FLUTTER_APPLICATION_PATH=C:\OneDriveCloud\OneDrive - Dikkan Gemi ve Endustriyel Vana Sanayi Ticaret A.S\Desktop\Flutter Apps\marketim"
-export "FLUTTER_TARGET=lib\main.dart"
+export "FLUTTER_ROOT=/Users/Zeybek/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/Zeybek/Desktop/Flutter Apps/marketim"
+export "FLUTTER_TARGET=/Users/Zeybek/Desktop/Flutter Apps/marketim/lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
-export "SYMROOT=${SOURCE_ROOT}/../build\ios"
-export "FLUTTER_FRAMEWORK_DIR=C:\Flutter\bin\cache\artifacts\engine\ios"
+export "SYMROOT=${SOURCE_ROOT}/../build/ios"
+export "OTHER_LDFLAGS=$(inherited) -framework Flutter"
+export "FLUTTER_FRAMEWORK_DIR=/Users/Zeybek/flutter/bin/cache/artifacts/engine/ios-profile"
 export "FLUTTER_BUILD_NAME=1.0.0"
 export "FLUTTER_BUILD_NUMBER=1"
+export "TRACK_WIDGET_CREATION=true"
